af876f2cec14b8df31b047db48d6b04d3dc9f4bc
Saif basheer <saif191020@gmail.com>
2020-08-23 14:12:04 +0530

    code refactored

--- Astar pathfinder.py
+++ Astar pathfinder.py
@@ -15,13 +15,12 @@ pygame.display.set_caption("A* PathFinding Algorithm")
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
-YELLOW = (255, 255, 0)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 PURPLE = (128, 0, 128)
 ORANGE = (255, 165 ,0)
 GREY = (128, 128, 128)
-TURQUOISE = (64, 224, 208)
+
 
 class Cube:
     def __init__(self, row, col, width, total_rows):
